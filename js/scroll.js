f7d55ecffd50370e322c6c5df40d489027512506
ZLordDabi <145398895+ZLordDabi@users.noreply.github.com>
2023-09-18 17:01:40 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,89 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.backimg2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-108px'
+    });
+window.sr = ScrollReveal();
+
+    sr.reveal('.backimg3', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-108px'
+    });
+window.sr = ScrollReveal();
+
+    sr.reveal('.backimg4', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-108px'
+    });
+window.sr = ScrollReveal();
+
+    sr.reveal('.backimg5', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-1008px'
+    });
+window.sr = ScrollReveal();
+
+    sr.reveal('.mkt1', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-200px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.mkt2', {
+        duration: 2500,
+        origin: 'right',
+        distance: '-1150px'
+    });
+window.sr = ScrollReveal();
+
+    sr.reveal('.hero__container', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.price__element', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.row1-container', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.row2-container', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.testimony__container', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    window.sr = ScrollReveal();
+
+    sr.reveal('.questions', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-1008px'
+    });
+    
+    
+    
+    
+    
---
+++ js/transition.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance: '650px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero__container',{delay:100, origin:'top'});
+sr.reveal('.hero',{delay:1, origin:'top'});
